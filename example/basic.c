c4171afb5be3c2926e96532af4c02cc86003d502
Marcin Borowicz <marcinbor85@gmail.com>
2021-04-06 00:48:52 +0200

    blocking io

--- example/basic.c
+++ example/basic.c
@@ -6,33 +6,6 @@
 
 #include "ush.h"
 
- 
-int kbhit(void)
-{
-        struct termios oldt, newt;
-        int ch;
-        int oldf;
-
-        tcgetattr(STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-        ch = getchar();
-
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-        if (ch != EOF) {
-                ungetc(ch, stdin);
-                return 1;
-        }
-
-        return 0;
-}
-
 static int write_char(struct ush_object *self, char ch)
 {
         (void)self;
@@ -44,13 +17,9 @@ static int write_char(struct ush_object *self, char ch)
 static int read_char(struct ush_object *self, char *ch)
 {
         (void)self;
-
-        if (kbhit()) {
-                *ch = getchar();
-                putc(*ch, stdout); /* echo */
-                return 1;
-        }
-        return 0;        
+     
+        *ch = getc(stdin);
+        return 1;
 }
 
 static const struct ush_iface g_ush_iface = {
