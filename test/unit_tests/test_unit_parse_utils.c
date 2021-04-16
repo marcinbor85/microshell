0797cc3bb4b107454cdfd9fd9c975b690cce5ad8
Marcin Borowicz <marcinbor85@gmail.com>
2021-04-17 01:59:49 +0200

    add parse utils unit tests

---
+++ test/unit_tests/test_unit_parse_utils.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <unity.h>
+
+#include "ush_internal.h"
+
+char input_buffer[128];
+struct ush_object ush;
+struct ush_descriptor ush_desc;
+
+void setUp(void)
+{
+        ush_desc.input_buffer = input_buffer;
+        ush_desc.input_buffer_size = sizeof(input_buffer);
+        
+        memset((uint8_t*)&ush, 0, sizeof(ush));
+        ush.desc = &ush_desc;
+}
+
+void tearDown(void)
+{
+
+}
+
+void test_ush_parse_start(void)
+{
+        for (int i = 0; i < USH_STATE__TOTAL_NUM; i++) {
+                ush_state_t state = (ush_state_t)i;
+
+                setUp();
+                ush.in_pos = 10;
+                ush.out_pos = 5;
+                ush.args_count = 2;
+                ush.escape_flag = true;
+                ush.state = state;
+
+                ush_parse_start(&ush);
+                TEST_ASSERT_EQUAL(USH_STATE_PARSE_SEARCH_ARG, ush.state);
+        }
+}
+
+void test_ush_parse_get_args_null(void)
+{
+        for (int i = 0; i < 10; i++) {
+                setUp();
+                ush.args_count = i;
+                TEST_ASSERT_EQUAL(i, ush_parse_get_args(&ush, NULL));
+        }
+}
+
+void test_ush_parse_get_args_data(void)
+{
+        char *argv[4];
+
+        setUp();
+        memset(argv, 0, sizeof(argv));
+        memcpy(input_buffer, "test\0", 5);
+        ush.args_count = 1;
+        TEST_ASSERT_EQUAL(1, ush_parse_get_args(&ush, argv));
+        TEST_ASSERT_EQUAL_STRING("test", argv[0]);
+        TEST_ASSERT_NULL(argv[1]);
+        TEST_ASSERT_NULL(argv[2]);
+        TEST_ASSERT_NULL(argv[3]);
+
+        setUp();
+        memset(argv, 0, sizeof(argv));
+        memcpy(input_buffer, "test\0abc\0", 9);
+        ush.args_count = 2;
+        TEST_ASSERT_EQUAL(2, ush_parse_get_args(&ush, argv));
+        TEST_ASSERT_EQUAL_STRING("test", argv[0]);
+        TEST_ASSERT_EQUAL_STRING("abc", argv[1]);
+        TEST_ASSERT_NULL(argv[2]);
+        TEST_ASSERT_NULL(argv[3]);
+
+        setUp();
+        memset(argv, 0, sizeof(argv));
+        memcpy(input_buffer, "test\0abc\0qwerty\0", 16);
+        ush.args_count = 3;
+        TEST_ASSERT_EQUAL(3, ush_parse_get_args(&ush, argv));
+        TEST_ASSERT_EQUAL_STRING("test", argv[0]);
+        TEST_ASSERT_EQUAL_STRING("abc", argv[1]);
+        TEST_ASSERT_EQUAL_STRING("qwerty", argv[2]);
+        TEST_ASSERT_NULL(argv[3]);
+
+        setUp();
+        memset(argv, 0, sizeof(argv));
+        memcpy(input_buffer, "test\0abc\0qwerty\0xyz\0", 20);
+        ush.args_count = 4;
+        TEST_ASSERT_EQUAL(4, ush_parse_get_args(&ush, argv));
+        TEST_ASSERT_EQUAL_STRING("test", argv[0]);
+        TEST_ASSERT_EQUAL_STRING("abc", argv[1]);
+        TEST_ASSERT_EQUAL_STRING("qwerty", argv[2]);
+        TEST_ASSERT_EQUAL_STRING("xyz", argv[3]);
+
+        setUp();
+        memset(argv, 0, sizeof(argv));
+        memcpy(input_buffer, "test abc\0qwerty xyz\0", 20);
+        ush.args_count = 2;
+        TEST_ASSERT_EQUAL(2, ush_parse_get_args(&ush, argv));
+        TEST_ASSERT_EQUAL_STRING("test abc", argv[0]);
+        TEST_ASSERT_EQUAL_STRING("qwerty xyz", argv[1]);
+        TEST_ASSERT_NULL(argv[2]);
+        TEST_ASSERT_NULL(argv[3]);
+}
+
+int main(int argc, char *argv[])
+{
+        (void)argc;
+        (void)argv;
+
+        UNITY_BEGIN();
+
+        RUN_TEST(test_ush_parse_start);
+        RUN_TEST(test_ush_parse_get_args_null);
+        RUN_TEST(test_ush_parse_get_args_data);
+
+        return UNITY_END();
+}
