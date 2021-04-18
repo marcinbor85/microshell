10076c616ba3e639ac77e7fb59bd05d22edd5d64
Marcin Borowicz <marcinbor85@gmail.com>
2021-04-18 16:02:58 +0200

    add commands modules unit tests

---
+++ test/unit_tests/test_unit_commands.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <unity.h>
+
+#include "ush_commands.h"
+
+struct ush_object ush;
+
+void setUp(void)
+{
+        memset((uint8_t*)&ush, 0, sizeof(ush));
+}
+
+void tearDown(void)
+{
+
+}
+
+void test_ush_commands_add(void)
+{
+        struct ush_node_object node = {0};
+        struct ush_file_descriptor files[4] = {0};
+
+        for (int i = 0; i < 4; i++) {
+                setUp();
+                node.path = "test";
+                ush.commands = (struct ush_node_object*)1234;
+                TEST_ASSERT_EQUAL(USH_STATUS_OK, ush_commands_add(&ush, &node, files, i));
+                TEST_ASSERT_EQUAL(&files, node.file_list);
+                TEST_ASSERT_EQUAL(i, node.file_list_size);
+                TEST_ASSERT_NULL(node.path);
+                TEST_ASSERT_EQUAL((struct ush_node_object*)1234, node.next);
+                TEST_ASSERT_EQUAL(&node, ush.commands);
+        }
+}
+
+void test_ush_commands_add_multi(void)
+{
+        struct ush_file_descriptor files;
+
+        struct ush_node_object cmd1 = {0};
+        struct ush_node_object cmd2 = {0};
+        struct ush_node_object cmd3 = {0};
+
+        ush.commands = NULL;
+        TEST_ASSERT_EQUAL(USH_STATUS_OK, ush_commands_add(&ush, &cmd1, &files, 1));
+        TEST_ASSERT_EQUAL(&cmd1, ush.commands);
+        TEST_ASSERT_NULL(cmd1.next);
+
+        TEST_ASSERT_EQUAL(USH_STATUS_OK, ush_commands_add(&ush, &cmd2, &files, 1));
+        TEST_ASSERT_EQUAL(&cmd2, ush.commands);
+        TEST_ASSERT_EQUAL(&cmd1, cmd2.next);
+        TEST_ASSERT_NULL(cmd1.next);
+
+        TEST_ASSERT_EQUAL(USH_STATUS_OK, ush_commands_add(&ush, &cmd3, &files, 1));
+        TEST_ASSERT_EQUAL(&cmd3, ush.commands);
+        TEST_ASSERT_EQUAL(&cmd2, cmd3.next);
+        TEST_ASSERT_EQUAL(&cmd1, cmd2.next);
+        TEST_ASSERT_NULL(cmd1.next);
+}
+
+void test_ush_commands_remove_not_exist_empty(void)
+{
+        struct ush_node_object node = {0};
+
+        ush.commands = NULL;
+        TEST_ASSERT_EQUAL(USH_STATUS_ERROR_NODE_NOT_FOUND, ush_commands_remove(&ush, &node));
+}
+
+void test_ush_commands_remove_not_exist_full(void)
+{
+        struct ush_node_object node = {0};
+        struct ush_node_object commands = {0};
+
+        ush.commands = &commands;
+        TEST_ASSERT_EQUAL(USH_STATUS_ERROR_NODE_NOT_FOUND, ush_commands_remove(&ush, &node));
+}
+
+
+void test_ush_commands_remove_exist_one(void)
+{
+        struct ush_node_object cmd1 = {0};
+
+        ush.commands = &cmd1;
+        TEST_ASSERT_EQUAL(USH_STATUS_OK, ush_commands_remove(&ush, &cmd1));
+        TEST_ASSERT_NULL(ush.commands);
+}
+
+void test_ush_commands_remove_exist_multi_first(void)
+{
+        struct ush_node_object cmd1 = {0};
+        struct ush_node_object cmd2 = {0};
+        struct ush_node_object cmd3 = {0};
+
+        cmd1.next = &cmd2;
+        cmd2.next = &cmd3;
+        cmd3.next = NULL;
+
+        ush.commands = &cmd1;
+        TEST_ASSERT_EQUAL(USH_STATUS_OK, ush_commands_remove(&ush, &cmd1));
+        TEST_ASSERT_EQUAL(&cmd2, ush.commands);
+        TEST_ASSERT_EQUAL(&cmd3, cmd2.next);
+        TEST_ASSERT_NULL(cmd3.next);
+}
+
+void test_ush_commands_remove_exist_multi_mid(void)
+{
+        struct ush_node_object cmd1 = {0};
+        struct ush_node_object cmd2 = {0};
+        struct ush_node_object cmd3 = {0};
+
+        cmd1.next = &cmd2;
+        cmd2.next = &cmd3;
+        cmd3.next = NULL;
+
+        ush.commands = &cmd1;
+        TEST_ASSERT_EQUAL(USH_STATUS_OK, ush_commands_remove(&ush, &cmd2));
+        TEST_ASSERT_EQUAL(&cmd1, ush.commands);
+        TEST_ASSERT_EQUAL(&cmd3, cmd1.next);
+        TEST_ASSERT_NULL(cmd3.next);
+}
+
+void test_ush_commands_remove_exist_multi_last(void)
+{
+        struct ush_node_object cmd1 = {0};
+        struct ush_node_object cmd2 = {0};
+        struct ush_node_object cmd3 = {0};
+
+        cmd1.next = &cmd2;
+        cmd2.next = &cmd3;
+        cmd3.next = NULL;
+
+        ush.commands = &cmd1;
+        TEST_ASSERT_EQUAL(USH_STATUS_OK, ush_commands_remove(&ush, &cmd3));
+        TEST_ASSERT_EQUAL(&cmd1, ush.commands);
+        TEST_ASSERT_EQUAL(&cmd2, cmd1.next);
+        TEST_ASSERT_NULL(cmd2.next);
+}
+
+int main(int argc, char *argv[])
+{
+        (void)argc;
+        (void)argv;
+
+        UNITY_BEGIN();
+
+        RUN_TEST(test_ush_commands_add);
+        RUN_TEST(test_ush_commands_add_multi);
+        RUN_TEST(test_ush_commands_remove_not_exist_empty);
+        RUN_TEST(test_ush_commands_remove_not_exist_full);
+        RUN_TEST(test_ush_commands_remove_exist_one);
+        RUN_TEST(test_ush_commands_remove_exist_multi_first);
+        RUN_TEST(test_ush_commands_remove_exist_multi_mid);
+        RUN_TEST(test_ush_commands_remove_exist_multi_last);
+
+        return UNITY_END();
+}
---
+++ test/unit_tests/test_unit_commands_buildin.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <unity.h>
+
+#include "ush_commands.h"
+
+struct ush_object ush;
+
+void setUp(void)
+{
+        memset((uint8_t*)&ush, 0, sizeof(ush));
+}
+
+void tearDown(void)
+{
+
+}
+
+void ush_buildin_cmd_help_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
+{
+        (void)self;
+        (void)file;
+        (void)argc;
+        (void)argv;
+}
+
+void ush_buildin_cmd_help_service(struct ush_object *self, struct ush_file_descriptor const *file)
+{
+        (void)self;
+        (void)file;
+}
+
+void ush_buildin_cmd_ls_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
+{
+        (void)self;
+        (void)file;
+        (void)argc;
+        (void)argv;
+}
+
+void ush_buildin_cmd_ls_service(struct ush_object *self, struct ush_file_descriptor const *file)
+{
+        (void)self;
+        (void)file;
+}
+
+void ush_buildin_cmd_cd_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
+{
+        (void)self;
+        (void)file;
+        (void)argc;
+        (void)argv;
+}
+
+void ush_buildin_cmd_pwd_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
+{
+        (void)self;
+        (void)file;
+        (void)argc;
+        (void)argv;
+}
+
+void ush_buildin_cmd_cat_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
+{
+        (void)self;
+        (void)file;
+        (void)argc;
+        (void)argv;
+}
+
+void ush_buildin_cmd_cat_service(struct ush_object *self, struct ush_file_descriptor const *file)
+{
+        (void)self;
+        (void)file;
+}
+
+void ush_buildin_cmd_xxd_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
+{
+        (void)self;
+        (void)file;
+        (void)argc;
+        (void)argv;
+}
+
+void ush_buildin_cmd_xxd_service(struct ush_object *self, struct ush_file_descriptor const *file)
+{
+        (void)self;
+        (void)file;
+}
+
+void ush_buildin_cmd_echo_callback(struct ush_object *self, struct ush_file_descriptor const *file, int argc, char *argv[])
+{
+        (void)self;
+        (void)file;
+        (void)argc;
+        (void)argv;
+}
+
+void test_g_ush_buildin_commands_help(void)
+{
+        struct ush_file_descriptor const *help = &g_ush_buildin_commands[0];
+
+        TEST_ASSERT_EQUAL_STRING("help", help->name);
+        TEST_ASSERT_NOT_NULL(help->description);
+        TEST_ASSERT_NOT_NULL(help->help);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_help_callback, help->exec);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_help_service, help->process);
+        TEST_ASSERT_NULL(help->get_data);
+        TEST_ASSERT_NULL(help->set_data);
+}
+
+void test_g_ush_buildin_commands_ls(void)
+{
+        struct ush_file_descriptor const *ls = &g_ush_buildin_commands[1];
+
+        TEST_ASSERT_EQUAL_STRING("ls", ls->name);
+        TEST_ASSERT_NOT_NULL(ls->description);
+        TEST_ASSERT_NOT_NULL(ls->help);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_ls_callback, ls->exec);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_ls_service, ls->process);
+        TEST_ASSERT_NULL(ls->get_data);
+        TEST_ASSERT_NULL(ls->set_data);
+}
+
+void test_g_ush_buildin_commands_cd(void)
+{
+        struct ush_file_descriptor const *cd = &g_ush_buildin_commands[2];
+
+        TEST_ASSERT_EQUAL_STRING("cd", cd->name);
+        TEST_ASSERT_NOT_NULL(cd->description);
+        TEST_ASSERT_NOT_NULL(cd->help);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_cd_callback, cd->exec);
+        TEST_ASSERT_NULL(cd->process);
+        TEST_ASSERT_NULL(cd->get_data);
+        TEST_ASSERT_NULL(cd->set_data);
+}
+
+void test_g_ush_buildin_commands_pwd(void)
+{
+        struct ush_file_descriptor const *pwd = &g_ush_buildin_commands[3];
+
+        TEST_ASSERT_EQUAL_STRING("pwd", pwd->name);
+        TEST_ASSERT_NOT_NULL(pwd->description);
+        TEST_ASSERT_NOT_NULL(pwd->help);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_pwd_callback, pwd->exec);
+        TEST_ASSERT_NULL(pwd->process);
+        TEST_ASSERT_NULL(pwd->get_data);
+        TEST_ASSERT_NULL(pwd->set_data);
+}
+
+void test_g_ush_buildin_commands_cat(void)
+{
+        struct ush_file_descriptor const *cat = &g_ush_buildin_commands[4];
+
+        TEST_ASSERT_EQUAL_STRING("cat", cat->name);
+        TEST_ASSERT_NOT_NULL(cat->description);
+        TEST_ASSERT_NOT_NULL(cat->help);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_cat_callback, cat->exec);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_cat_service, cat->process);
+        TEST_ASSERT_NULL(cat->get_data);
+        TEST_ASSERT_NULL(cat->set_data);
+}
+
+void test_g_ush_buildin_commands_xxd(void)
+{
+        struct ush_file_descriptor const *xxd = &g_ush_buildin_commands[5];
+
+        TEST_ASSERT_EQUAL_STRING("xxd", xxd->name);
+        TEST_ASSERT_NOT_NULL(xxd->description);
+        TEST_ASSERT_NOT_NULL(xxd->help);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_xxd_callback, xxd->exec);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_xxd_service, xxd->process);
+        TEST_ASSERT_NULL(xxd->get_data);
+        TEST_ASSERT_NULL(xxd->set_data);
+}
+
+void test_g_ush_buildin_commands_echo(void)
+{
+        struct ush_file_descriptor const *echo = &g_ush_buildin_commands[6];
+
+        TEST_ASSERT_EQUAL_STRING("echo", echo->name);
+        TEST_ASSERT_NOT_NULL(echo->description);
+        TEST_ASSERT_NOT_NULL(echo->help);
+        TEST_ASSERT_EQUAL(ush_buildin_cmd_echo_callback, echo->exec);
+        TEST_ASSERT_NULL(echo->process);
+        TEST_ASSERT_NULL(echo->get_data);
+        TEST_ASSERT_NULL(echo->set_data);
+}
+
+void test_g_ush_buildin_commands_num(void)
+{
+        TEST_ASSERT_EQUAL(7, g_ush_buildin_commands_num);
+}
+
+int main(int argc, char *argv[])
+{
+        (void)argc;
+        (void)argv;
+
+        UNITY_BEGIN();
+
+        RUN_TEST(test_g_ush_buildin_commands_help);
+        RUN_TEST(test_g_ush_buildin_commands_ls);
+        RUN_TEST(test_g_ush_buildin_commands_cd);
+        RUN_TEST(test_g_ush_buildin_commands_pwd);
+        RUN_TEST(test_g_ush_buildin_commands_cat);
+        RUN_TEST(test_g_ush_buildin_commands_xxd);
+        RUN_TEST(test_g_ush_buildin_commands_echo);
+        RUN_TEST(test_g_ush_buildin_commands_num);
+
+        return UNITY_END();
+}
