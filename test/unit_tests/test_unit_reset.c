ae9791bbf8bb5f18f266fde0beb0de073c170bdc
Marcin Borowicz <marcinbor85@gmail.com>
2021-05-02 10:46:14 +0200

    fix typo

--- test/unit_tests/test_unit_reset.c
+++ test/unit_tests/test_unit_reset.c
@@ -14,7 +14,7 @@ int ush_prompt_start_call_count;
 
 char *ush_write_pointer_text;
 ush_state_t ush_write_pointer_state;
-ush_state_t ush_prompt_state_state;
+ush_state_t ush_prompt_start_state;
 
 void setUp(void)
 {
@@ -25,7 +25,7 @@ void setUp(void)
 
         ush_write_pointer_text = NULL;
         ush_write_pointer_state = USH_STATE__TOTAL_NUM;
-        ush_prompt_state_state = USH_STATE__TOTAL_NUM;
+        ush_prompt_start_state = USH_STATE__TOTAL_NUM;
 }
 
 void tearDown(void)
@@ -45,7 +45,7 @@ void ush_write_pointer(struct ush_object *self, char *text, ush_state_t state)
 void ush_prompt_start(struct ush_object *self, ush_state_t prepare_next_state)
 {
         TEST_ASSERT_EQUAL(&ush, self);
-        TEST_ASSERT_EQUAL(ush_prompt_state_state, prepare_next_state);
+        TEST_ASSERT_EQUAL(ush_prompt_start_state, prepare_next_state);
 
         ush_prompt_start_call_count++;
 }
@@ -92,7 +92,7 @@ void test_ush_reset_service(void)
                         TEST_ASSERT_EQUAL(0, ush_prompt_start_call_count);
                         break;
                 case USH_STATE_RESET_PROMPT:
-                        ush_prompt_state_state = USH_STATE_READ_PREPARE;
+                        ush_prompt_start_state = USH_STATE_READ_PREPARE;
                         TEST_ASSERT_TRUE(ush_reset_service(&ush));
                         TEST_ASSERT_EQUAL(0, ush_write_pointer_call_count);
                         TEST_ASSERT_EQUAL(1, ush_prompt_start_call_count);
