d3d208f1a1d72973fe2bc1f8be9fbb6bf97e3496
Marcin Borowicz <marcinbor85@gmail.com>
2021-05-05 17:48:37 +0200

    add port layer

---
+++ src/inc/ush_preconfig.h
@@ -0,0 +1,54 @@
+/*
+MIT License
+
+Copyright (c) 2021 Marcin Borowicz <marcinbor85@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef USH_PRECONFIG_H
+#define USH_PRECONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if !defined(USH_CONFIG_CUSTOM_FILE)
+
+#if defined(ARDUINO)
+        #if defined(ARDUINO_ARCH_AVR)
+                #include "port/arduino/avr/ush_config.h"
+        #else
+                #error "Unsupported ARDUINO_ARCH"
+        #endif
+#elif defined(USH_CONFIG_PORT_POSIX)
+        #include "port/posix/ush_config.h"
+#else
+        #include "ush_config.h"
+#endif
+
+#else
+        #include USH_CONFIG_CUSTOM_FILE
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USH_PRECONFIG_H */
---
+++ src/port/arduino/avr/ush_config.h
@@ -0,0 +1,65 @@
+
+#ifndef USH_CONFIG_H
+#define USH_CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(ARDUINO_AVR_MEGA2560)
+
+#define USH_CONFIG_PATH_MAX_LENGTH                              128
+#define USH_CONFIG_ENABLE_COMMAND_CAT                           1
+#define USH_CONFIG_ENABLE_COMMAND_CD                            1
+#define USH_CONFIG_ENABLE_COMMAND_ECHO                          1
+#define USH_CONFIG_ENABLE_COMMAND_HELP                          1
+#define USH_CONFIG_ENABLE_COMMAND_LS                            1
+#define USH_CONFIG_ENABLE_COMMAND_PWD                           1
+#define USH_CONFIG_ENABLE_COMMAND_XXD                           1
+
+#elif defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO)
+
+#define USH_CONFIG_PATH_MAX_LENGTH                              32
+#define USH_CONFIG_ENABLE_COMMAND_CAT                           1
+#define USH_CONFIG_ENABLE_COMMAND_CD                            0
+#define USH_CONFIG_ENABLE_COMMAND_ECHO                          0
+#define USH_CONFIG_ENABLE_COMMAND_HELP                          1
+#define USH_CONFIG_ENABLE_COMMAND_LS                            1
+#define USH_CONFIG_ENABLE_COMMAND_PWD                           0
+#define USH_CONFIG_ENABLE_COMMAND_XXD                           0
+
+#else
+
+#error "Unsupported ARDUINO_AVR board"
+
+#endif
+
+#define USH_CONFIG_FILENAME_ALIGN_SPACE                         12
+#define USH_CONFIG_CMD_XXD_COLUMNS                              8
+
+#define USH_CONFIG_ENABLE_FEATURE_COMMANDS                      1
+#define USH_CONFIG_ENABLE_FEATURE_AUTOCOMPLETE                  1
+#define USH_CONFIG_ENABLE_FEATURE_SHELL_STYLES                  1
+
+#define USH_CONFIG_TRANSLATION_OK                               "ok"
+#define USH_CONFIG_TRANSLATION_ERROR                            "error"
+#define USH_CONFIG_TRANSLATION_DIRECTORY_NOT_FOUND              "directory not found"
+#define USH_CONFIG_TRANSLATION_NESTED_DIRECTORIES_EXIST         "nested directories exist"
+#define USH_CONFIG_TRANSLATION_CANNOT_FIND_PARENT_NODE          "cannot find parent node"
+#define USH_CONFIG_TRANSLATION_DIRECTORY_ALREADY_MOUNTED        "directory already mounted"
+#define USH_CONFIG_TRANSLATION_SYNTAX_ERROR                     "syntax error"
+#define USH_CONFIG_TRANSLATION_WRONG_ARGUMENTS                  "wrong arguments"
+#define USH_CONFIG_TRANSLATION_FILE_NOT_EXECUTABLE              "file not executable"
+#define USH_CONFIG_TRANSLATION_FILE_NOT_WRITABLE                "file not writable"
+#define USH_CONFIG_TRANSLATION_FILE_NOT_READABLE                "file not readable"
+#define USH_CONFIG_TRANSLATION_NO_HELP_AVAILABLE                "no help available"
+#define USH_CONFIG_TRANSLATION_FILE_NOT_FOUND                   "file not found"
+#define USH_CONFIG_TRANSLATION_READ_ONLY_FILE                   "read only file"
+
+#define USH_ASSERT(cond) { }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USH_CONFIG_PORT_H */
